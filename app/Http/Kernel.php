5157b74fc4bbae7a31d4f4f6b9d00d8e5a0bae8c
You Plus We <official@youpluswe.com>
2018-06-26 21:21:06 +0300

    disable HTTPs redirection

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
-        'App\Http\Middleware\HttpsProtocol' 
+        //'App\Http\Middleware\HttpsProtocol' 
     ];
 
     /**
