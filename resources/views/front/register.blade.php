7e3fa40c5c80446dfb5e8e145b61ef0e6b02d1f1
Alex <official@youpluswe.com>
2018-09-25 11:07:50 +0300

    enable registrations

--- resources/views/front/register.blade.php
+++ resources/views/front/register.blade.php
@@ -80,11 +80,12 @@
 
 						<div {!! count($errors) ? '' : 'style="display: none;"' !!} id="register-div">
 
+						<!--
 						<div class="alert alert-warning">
 							Due to the overwhelming surge in popularity, new registrations on Trusted Review Platform are currently disabled to allow for infrastructure & security upgrades. Thank you for your understanding!
 						</div>
+						-->
 
-							<!--
 							<div id="step-1">
 								<p>
 									{{ trans('front.page.'.$current_page.'.step-1-hint') }}								
@@ -302,7 +303,6 @@
 								</div>
 							</div>
 
-						-->
 
 						</div>
 
