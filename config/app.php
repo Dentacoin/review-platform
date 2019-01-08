2b7fd0230bd4168bc837481a24c9852a97ead5fb
Alex <official@youpluswe.com>
2019-01-08 17:37:01 +0200

    remove debug output (just to be sure)

--- config/app.php
+++ config/app.php
@@ -39,6 +39,11 @@ return [
     */
 
     'debug' => env('APP_DEBUG', true),
+    'debug_blacklist' => [
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV),        
+    ],
 
     /*
     |--------------------------------------------------------------------------
