bb46e64b38e681cbe5494e185353e8d57efcaf07
Gergana <gergana@youpluswe.com>
2019-01-18 14:57:04 +0200

    admin surveys sorted fields fixes

--- resources/views/admin/parts/table-voxes-featured.blade.php
+++ resources/views/admin/parts/table-voxes-featured.blade.php
@@ -1,3 +1,4 @@
+<span style="font-size: 0px;">{{ intval($item->featured) }}</span>
 @if($item->type=='normal' || $item->type=='hidden')
 	<input type="checkbox" class="toggler " field="featured" id="{{ $item->id }}" {!! $item->featured ? 'checked="checked"' : '' !!} >
 @else
--- resources/views/admin/parts/table-voxes-stats.blade.php
+++ resources/views/admin/parts/table-voxes-stats.blade.php
@@ -1,1 +1,2 @@
+<span style="font-size: 0px;">{{ intval($item->has_stats) }}</span>
 <input type="checkbox" class="toggler" field="has_stats" id="{{ $item->id }}" {!! $item->has_stats ? 'checked="checked"' : '' !!} />
