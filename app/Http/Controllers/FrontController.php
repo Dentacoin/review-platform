74d8a41b41097ed69d5ff3c6ac9e33b45dae9294
Gergana <gergana@youpluswe.com>
2019-02-12 15:05:03 +0200

    new cache version

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -351,7 +351,7 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '20190212';
+        $params['cache_version'] = '20190212-1';
     }
 
 }
