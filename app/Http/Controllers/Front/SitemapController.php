b161ff9a41ad5d2e3f13f76e134cf5ff9daff398
Gergana <gergana@youpluswe.com>
2019-02-14 12:20:59 +0200

    trp sitemap changes

--- app/Http/Controllers/Front/SitemapController.php
+++ app/Http/Controllers/Front/SitemapController.php
@@ -25,12 +25,17 @@ class SitemapController extends FrontController
 			$links[] = $dentist->getLink();
 		}
 
-		$dentists = User::where('is_dentist', 1)->whereNotNull('country_id')->where('status', 'approved')->groupBy('country_id')->get()->pluck('country_id');
+		$dentists = User::where('is_dentist', 1)->whereNotNull('country_id')->where('status', 'approved')->whereNotNull('city_name')->groupBy('country_id')->get()->pluck('country_id');
         $dentist_countries = Country::whereIn('id', $dentists )->get();
+        $dentist_cities = User::where('is_dentist', 1)->where('status', 'approved')->whereNotNull('country_id')->whereNotNull('city_name')->groupBy('city_name')->get();
 
         foreach ($dentist_countries as $country) {
         	$links[] = getLangUrl('dentists-in-'.$country->slug);
         }
+
+    	foreach ($dentist_cities as $user) {
+    		$links[] = getLangUrl('dentists/'.str_replace(' ', '-', strtolower($user->city_name)).'-'.$user->country->slug);
+    	}
 
 		$content = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">';
 		foreach ($links as $link) {
