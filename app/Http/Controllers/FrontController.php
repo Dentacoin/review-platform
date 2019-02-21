f6722dfd3287ab7a4624a253e9e412d87f8e992f
Alex <official@youpluswe.com>
2019-02-21 19:51:38 +0200

    clear cache

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -375,7 +375,7 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '20190218-3';
+        $params['cache_version'] = '20190221';
     }
 
 }
