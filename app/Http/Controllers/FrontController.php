80a8e3f097ee7402e797d4e141083e8a410f9e3d
Gergana <gergana@youpluswe.com>
2019-02-27 11:46:29 +0200

    cms changes

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -375,7 +375,7 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '20190226-1';
+        $params['cache_version'] = '20190227';
     }
 
 }
