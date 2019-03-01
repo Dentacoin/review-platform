4732d795cb5463f46d48174c1b25822303ff8e52
Alex <official@youpluswe.com>
2019-03-01 17:52:48 +0200

    remove soft deletes from rewards

--- app/Models/VoxReward.php
+++ app/Models/VoxReward.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+//use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VoxReward extends Model {
     
-    use SoftDeletes;
+    //use SoftDeletes;
     
     protected $fillable = [
         'user_id',
