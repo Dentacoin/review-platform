6ec24f46b2cf9bfce96b88cd612be0563a29436f
Alex <official@youpluswe.com>
2019-04-01 10:50:24 +0300

    add Kosovo

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -5,12 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Dimsav\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Builder;
 
 use App\Models\City;
 
 class Country extends Model
 {
     use SoftDeletes, Translatable;
+
+    protected static function boot()
+    {
+        parent::boot();
+     
+        // Order by name ASC
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('name', 'asc');
+        });
+    }
     
     public $translatedAttributes = [
         "name"
