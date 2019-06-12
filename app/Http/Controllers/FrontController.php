4b6de1a89674dbf980360cc666311bc983558883
Gergana <gergana@youpluswe.com>
2019-06-12 18:24:44 +0300

    design changes

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -538,6 +538,6 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '2019-06-12-01';
+        $params['cache_version'] = '2019-06-12-02';
     }
 }
