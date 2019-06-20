93bffbc1a4071c94266c3bbf699b1bdfb667e2b5
Gergana <gergana@youpluswe.com>
2019-06-20 11:42:17 +0300

    vox bug

--- app/Http/Controllers/Vox/LoginController.php
+++ app/Http/Controllers/Vox/LoginController.php
@@ -275,7 +275,7 @@ class LoginController extends FrontController
             $data = json_decode($civic->response, true);
             if(!empty($data['userId'])) {
 
-                dd($data);
+                //dd($data);
                 $email = null;
                 $phone = null;
 
@@ -321,7 +321,7 @@ class LoginController extends FrontController
 
 
                         if ($user) {
-                            if($user->loggedFromBadIp()) {
+                            if(true || $user->loggedFromBadIp()) {
                                 
                                 $ret['success'] = false;
                                 $ret['popup'] = 'suspended-popup';
