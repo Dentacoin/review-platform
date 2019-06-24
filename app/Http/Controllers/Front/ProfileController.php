68a50c674610dd06d56cd19df847437910d637ff
Gergana <gergana@youpluswe.com>
2019-06-24 12:41:05 +0300

    new changes

--- app/Http/Controllers/Front/ProfileController.php
+++ app/Http/Controllers/Front/ProfileController.php
@@ -468,6 +468,7 @@ Link to user\'s profile in CMS: https://reviews.dentacoin.com/cms/users/edit/'.$
                     }
 
                     //Mega hack
+                    $user_dentist = $this->user;
                     $dentist_name = $this->user->name;
                     $dentist_email = $this->user->email;
                     $this->user->name = Request::Input('name');
@@ -476,10 +477,10 @@ Link to user\'s profile in CMS: https://reviews.dentacoin.com/cms/users/edit/'.$
 
 
                     $substitutions = [
-                        'type' => $this->user->is_clinic ? 'dental clinic' : ($this->user->is_dentist ? 'your dentist' : ''),
-                        'inviting_user_name' => $this->user->name,
-                        'invited_user_name' => Request::Input('name'),
-                        "invitation_link" => getLangUrl('invite/'.$this->user->id.'/'.$this->user->get_invite_token().'/'.$invitation->id, null, 'https://reviews.dentacoin.com/'),
+                        'type' => $user_dentist->is_clinic ? 'dental clinic' : ($user_dentist->is_dentist ? 'your dentist' : ''),
+                        'inviting_user_name' => $user_dentist->name,
+                        'invited_user_name' => $this->user->name,
+                        "invitation_link" => getLangUrl('invite/'.$user_dentist->id.'/'.$user_dentist->get_invite_token().'/'.$invitation->id, null, 'https://reviews.dentacoin.com/'),
                     ];
 
                     $this->user->sendGridTemplate(59, $substitutions);
