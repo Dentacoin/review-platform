0d5b513b6f297d9912f16b4e0c2174cf3c58fb0b
Gergana <gergana@youpluswe.com>
2019-06-24 13:36:54 +0300

    vox login bug fixes

--- resources/views/vox/login.blade.php
+++ resources/views/vox/login.blade.php
@@ -25,7 +25,7 @@
 						<p>
 							{!! nl2br(trans('vox.page.auth.before-login.hint')) !!}
 						</p>
-						<form action="{{ getVoxUrl('login') }}" method="post" class="form-horizontal">
+						<form action="{{ getLangUrl('login', null, 'https://dentavox.dentacoin.com/') }}" method="post" class="form-horizontal">
 							{!! csrf_field() !!}
 
 							<div class="modern-field alert-after">
