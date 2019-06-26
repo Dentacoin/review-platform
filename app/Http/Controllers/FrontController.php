ef6612d4ce72cfa8de39b36b4812e381fe093dd0
Alex <official@youpluswe.com>
2019-06-26 13:20:47 +0300

    TRP profile dentist reviews

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -538,6 +538,6 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '2019-06-25';
+        $params['cache_version'] = '2019-06-26';
     }
 }
