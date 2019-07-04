9661b08bfd9fec1fccb48ad12934346c8989d1d3
Gergana <gergana@youpluswe.com>
2019-07-04 16:06:46 +0300

    trp bug fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -1977,7 +1977,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
             $font->align('left');
             $font->valign('top');
         });
-        $location = $this->city->name.', '.$this->country->name;
+        $location = $this->address.', '.$this->country->name;
         $img->text($location, 562, 365, function($font) {
             $font->file(public_path().'/fonts/Calibri.ttf');
             $font->size(30);
