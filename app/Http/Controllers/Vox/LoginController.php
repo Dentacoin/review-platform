1dc8d022ee3106c1f924ba1d684629f031786e2e
Gergana <gergana@youpluswe.com>
2019-07-10 12:25:03 +0300

    vox login bug fixes

--- app/Http/Controllers/Vox/LoginController.php
+++ app/Http/Controllers/Vox/LoginController.php
@@ -347,7 +347,7 @@ class LoginController extends FrontController
 
                                 $intended = session()->pull('our-intended');
 
-                                return redirect($user->isBanned('vox') ? getVoxUrl('profile') : ($intended ? $intended : getVoxUrl('/')));
+                                return redirect($user->isBanned('vox') ? getVoxUrl('profile') : ($intended ? $intended : ( Request::input('intended') ? Request::input('intended') : getVoxUrl('/'))));
                                 
                                 $sess = [
                                     'login_patient' => true,
