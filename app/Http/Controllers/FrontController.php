f31d20b4a96cbe0e8e60e97c1f0dd84cbd742fd9
Gergana <gergana@youpluswe.com>
2019-07-10 16:06:34 +0300

    vox bug fixes

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -98,11 +98,7 @@ class FrontController extends BaseController
             if ($this->user && session('intended')) {
                 $intended = session()->pull('intended');
 
-                if (User::getRealIp() == '78.130.213.163') {
-                    dd($intended);
-                }
-
-                return redirect($intended);
+                Redirect::to($intended)->send();
             }
 
             if($this->user && session('login-logged')!=$this->user->id){
