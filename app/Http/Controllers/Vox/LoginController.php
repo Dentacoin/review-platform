2048dda869b944cd9e006bd57bcf138f3732d527
Gergana <gergana@youpluswe.com>
2019-07-10 12:19:06 +0300

    vox login bug fixes

--- app/Http/Controllers/Vox/LoginController.php
+++ app/Http/Controllers/Vox/LoginController.php
@@ -85,8 +85,7 @@ class LoginController extends FrontController
                 if( $path  ){
                     return redirect( getVoxUrl($path) );
                 }
-                
-                return redirect( $intended ? $intended : ( $request->input('intended') ? $request->input('intended') : getVoxUrl('/')) );
+                return redirect( $intended ? $intended : getVoxUrl('/') );
             } else {
                 //dd('Other error', $s_user, $s_user->user);
                 Request::session()->flash('error-message', trans('vox.page.login.error-fb', [
