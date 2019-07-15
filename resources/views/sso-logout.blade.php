75585a853e77822a79b584bc7d46307901571bc6
Gergana <gergana@youpluswe.com>
2019-07-15 15:36:14 +0300

    bug

--- resources/views/sso-logout.blade.php
+++ resources/views/sso-logout.blade.php
@@ -1,6 +1,6 @@
 @php($token = (new \App\Http\Controllers\SSOController())->encrypt($markLogout))
 @foreach( config('platforms') as $k => $platform )
-	@if( $platform['url'] && ( mb_strpos(request()->getHttpHost(), $platform['url'])===false || $platform['url']=='dentacoin.com' )  )
+	@if( !empty($platform['url']) && ( mb_strpos(request()->getHttpHost(), $platform['url'])===false || $platform['url']=='dentacoin.com' )  )
 		<img src="//{{ $platform['url'] }}/custom-cookie?logout-token={{ urlencode($token) }}" class="hide"/>
 	@endif
 @endforeach
