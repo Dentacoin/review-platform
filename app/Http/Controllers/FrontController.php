3fcecde8f72d10640cc2144433f8e69887bd9082
Gergana <gergana@youpluswe.com>
2019-08-06 09:55:28 +0300

    vox new image

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -561,6 +561,6 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '2019-08-01-01';
+        $params['cache_version'] = '2019-08-06';
     }
 }
