bfff328bdf9f9c4e51159a7ae7bf4d74429c2b14
Gergana <gergana@youpluswe.com>
2019-09-11 13:36:20 +0300

    cron bug fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -59,6 +59,7 @@ class Kernel extends ConsoleKernel
                 $list = IncompleteRegistration::whereNull('completed')->whereNull('unsubscribed')->whereNull( $field )->where('created_at', '<', $time['time'])->get();
                 foreach ($list as $notify) {
                     if (!empty($notify->email)) {
+                        echo 'USER: '.$notify;
                         echo 'USER EMAIL: '.$notify->email;
                         $u = User::find(3);
                         $tmpEmail = $u->email;
