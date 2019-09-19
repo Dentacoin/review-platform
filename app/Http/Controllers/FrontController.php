faf50ce4ab5dcd2fe13127f130222b90507fa61c
Gergana <gergana@youpluswe.com>
2019-09-19 12:59:13 +0300

    trp design fixes

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -631,6 +631,6 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '2019-09-19-01';
+        $params['cache_version'] = '2019-09-19-02';
     }
 }
