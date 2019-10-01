84b34c44464bcfb60999f9651c16ce34621eabf3
Gergana <gergana@youpluswe.com>
2019-10-01 10:56:34 +0300

    vox daily polls design fixes

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -631,6 +631,6 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '2019-09-30-01';
+        $params['cache_version'] = '2019-10-01';
     }
 }
