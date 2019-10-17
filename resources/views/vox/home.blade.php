0091a020a0817f5f87e33990600e55d42de49e19
Gergana <gergana@youpluswe.com>
2019-10-17 10:20:33 +0300

    vox bug fixes

--- resources/views/vox/home.blade.php
+++ resources/views/vox/home.blade.php
@@ -134,7 +134,7 @@
 			      			>
 				      		<div class="slider-inner">
 					    		<div class="slide-padding">
-					      			<a href="{{ !empty($taken) && in_array($vox->id, $taken) ? 'javascript:;' : $vox->getLink() }}" class="cover" style="background-image: url('{{ $vox->getImageUrl() }}');">
+					      			<a href="{{ !empty($taken) && in_array($vox->id, $taken) ? 'javascript:;' : $vox->getLink() }}" class="cover" style="background-image: url('{{ $vox->getImageUrl() }}');" alt='{{ trans("vox.page.stats.title-single", ["name" => $vox->title ]) }}'>
 					      				@if($vox->featured)
 					      					<img class="featured-img doublecoin" src="{{ url('new-vox-img/flipping-coin.gif') }}">
 					      				@endif
