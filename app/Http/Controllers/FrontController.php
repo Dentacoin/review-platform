e7ec3168e77f5e3da048d93a86a9862c3b8ad762
Gergana <gergana@youpluswe.com>
2019-10-30 14:26:15 +0200

    vox design fixes

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -662,6 +662,6 @@ class FrontController extends BaseController
             }
         }
 
-        $params['cache_version'] = '2019-10-30-02';
+        $params['cache_version'] = '2019-10-30-03';
     }
 }
