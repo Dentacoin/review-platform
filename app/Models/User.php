7de932c708f516fbba4eaf609eda71a9d33895a4
Gergana <gergana@youpluswe.com>
2019-12-11 17:00:02 +0200

    users delete actions bug fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -1172,7 +1172,7 @@ Link to user\'s profile in CMS: https://reviews.dentacoin.com/cms/users/edit/'.$
 
         $transactions = DcnTransaction::where('user_id', $this->id)->whereIn('status', ['unconfirmed', 'new', 'failed', 'first'])->get();
 
-        if (!empty($transactions)) {
+        if ($transactions->isNotEmpty()) {
             foreach ($transactions as $trans) {
                 $trans->status = 'stopped';
                 $trans->save();
