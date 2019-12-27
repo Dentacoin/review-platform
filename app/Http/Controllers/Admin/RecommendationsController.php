bc1b98b68660f222c90ca3d2c9a0a328f433be18
Gergana <gergana@youpluswe.com>
2019-12-27 17:31:14 +0200

    admin improvement

--- app/Http/Controllers/Admin/RecommendationsController.php
+++ app/Http/Controllers/Admin/RecommendationsController.php
@@ -33,24 +33,58 @@ class RecommendationsController extends AdminController
             $recommendations = $recommendations->where('scale', $this->request->input('search-scale'));
         }
 
-        if( null !== $this->request->input('results-number')) {
-            $results = trim($this->request->input('results-number'));
+        $total_count = $recommendations->count();
+
+        $page = max(1,intval(request('page')));
+        
+        $ppp = 100;
+        $adjacents = 2;
+        $total_pages = ceil($total_count/$ppp);
+
+        //Here we generates the range of the page numbers which will display.
+        if($total_pages <= (1+($adjacents * 2))) {
+          $start = 1;
+          $end   = $total_pages;
         } else {
-            $results = 500;
+          if(($page - $adjacents) > 1) { 
+            if(($page + $adjacents) < $total_pages) { 
+              $start = ($page - $adjacents);            
+              $end   = ($page + $adjacents);         
+            } else {             
+              $start = ($total_pages - (1+($adjacents*2)));  
+              $end   = $total_pages;               
+            }
+          } else {               
+            $start = 1;                                
+            $end   = (1+($adjacents * 2));             
+          }
         }
 
-        if($results == 0) {
-            $recommendations = $recommendations->take(1000)->get();
-        } else {
-            $recommendations = $recommendations->take($results)->get();
-        }
+        $recommendations = $recommendations->skip( ($page-1)*$ppp )->take($ppp)->get();
+
+        //If you want to display all page links in the pagination then
+        //uncomment the following two lines
+        //and comment out the whole if condition just above it.
+        /*$start = 1;
+        $end = $total_pages;*/
+
+        $current_url = url('cms/vox/recommendations');
+
+        $pagination_link = (!empty($this->request->input('search-user-id')) ? '&search-user-id='.$this->request->input( 'search-user-id' ) : '').(!empty($this->request->input('search-name-user')) ? '&search-name-user='.$this->request->input( 'search-name-user' ) : '').(!empty($this->request->input('search-scale')) ? '&search-scale='.$this->request->input( 'search-scale' ) : '');
 
         return $this->showView('recommendations', array(
             'recommendations' => $recommendations,
             'search_name_user' => $this->request->input('search-name-user'),
             'search_user_id' => $this->request->input('search-user-id'),
             'search_scale' => $this->request->input('search-scale'),
-            'results_number' => $this->request->input('results-number'),
+            'total_count' => $total_count,
+            'count' =>($page - 1)*$ppp ,
+            'start' => $start,
+            'end' => $end,
+            'total_pages' => $total_pages,
+            'page' => $page,
+            'pagination_link' => $pagination_link,
+            'current_url' => $current_url,
         ));
     }
 
--- resources/views/admin/recommendations.blade.php
+++ resources/views/admin/recommendations.blade.php
@@ -23,11 +23,8 @@
                     <div class="col-md-2">
                         <input type="text" class="form-control" name="search-name-user" value="{{ $search_name_user }}" placeholder="User name">
                     </div>
-                    <div class="col-md-1">
-                        <input type="text" class="form-control" name="search-scale" value="{{ $search_scale }}" placeholder="Scale number">
-                    </div>
                     <div class="col-md-2">
-                        <input type="text" class="form-control" name="results-number" value="{{ $results_number }}" placeholder="Results ( enter 0 to show all )">
+                        <input type="text" class="form-control" name="search-scale" value="{{ $search_scale }}" placeholder="Scale number">
                     </div>
                     <input type="submit" class="btn btn-sm btn-primary col-md-1" name="search" value="Search">
                 </form>
@@ -48,20 +45,54 @@
                 <h4 class="panel-title">{{ trans('admin.page.'.$current_page.'.title') }}</h4>
             </div>
             <div class="panel-body">
-				@include('admin.parts.table', [
-					'table_id' => 'users',
-					'table_fields' => [
-                        'created_at'        => array('format' => 'datetime'),
-                        'user'              => array('template' => 'admin.parts.table-recommend-user','label' => 'Name'),
-                        'scale'            => array('label' => 'Scale'),
-                        'description'       => array('format' => 'break-word','label' => 'Comment'),
-					],
-                    'table_data' => $recommendations,
-					'table_pagination' => false,
-                    'pagination_link' => array()
-				])
+                <div>
+                    Recommendations count: {{ $total_count }}
+                </div>
+                <div class="panel-body">
+    				@include('admin.parts.table', [
+    					'table_id' => 'users',
+    					'table_fields' => [
+                            'created_at'        => array('format' => 'datetime'),
+                            'user'              => array('template' => 'admin.parts.table-recommend-user','label' => 'Name'),
+                            'scale'            => array('label' => 'Scale'),
+                            'description'       => array('format' => 'break-word','label' => 'Comment'),
+    					],
+                        'table_data' => $recommendations,
+    					'table_pagination' => false,
+                        'pagination_link' => array()
+    				])
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+@if($total_pages > 1)
+    <nav aria-label="Page navigation" style="text-align: center;">
+        <ul class="pagination">
+            <li class="{{ ($page <= 1 ?  'disabled' : '' ) }}">
+                <a class="page-link" href="{{ url('cms/vox/recommendations/?page=1'.$pagination_link) }}" aria-label="Previous">
+                    <span aria-hidden="true"> << </span>
+                </a>
+            </li>
+            <li class="{{ ($page <= 1 ?  'disabled' : '' ) }}">
+                <a class="page-link prev" href="{{ url('cms/vox/recommendations/?page='.($page>1 ? $page-1 : '1').$pagination_link) }}"  aria-label="Previous">
+                    <span aria-hidden="true"> < </span>
+                </a>
+            </li>
+            @for($i=$start; $i<=$end; $i++)
+                <li class="{{ ($i == $page ?  'active' : '') }}">
+                    <a class="page-link" href="{{ url('cms/vox/recommendations/?page='.$i.$pagination_link) }}">{{ $i }}</a>
+                </li>
+            @endfor
+            <li class="{{ ($page >= $total_pages ? 'disabled' : '') }}">
+                <a class="page-link next" href="{{ url('cms/vox/recommendations/?page='.($page < $total_pages ? $page+1 :  $total_pages).$pagination_link) }}" aria-label="Next"> <span aria-hidden="true"> > </span> </a>
+            </li>
+            <li class="{{ ($page >= $total_pages ? 'disabled' : '') }}">
+                <a class="page-link" href="{{ url('cms/vox/recommendations/?page='.$total_pages.$pagination_link) }}" aria-label="Next"> <span aria-hidden="true"> >> </span>  </a>
+            </li>
+        </ul>
+    </nav>
+@endif
+
 @endsection
