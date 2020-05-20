f4fd7ab51f4a3d9c5ae606388f4b86778694fe50
Gergana <gergana@youpluswe.com>
2020-05-20 15:18:47 +0300

    bug fixes

--- resources/views/trp.blade.php
+++ resources/views/trp.blade.php
@@ -4,7 +4,10 @@
         <base href="{{ url('/') }}">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="height=device-height, 
+                      width=device-width, initial-scale=1.0, 
+                      minimum-scale=1.0, maximum-scale=1.0, 
+                      user-scalable=no, target-densitydpi=device-dpi">
         <meta name="google-site-verification" content="b0VE72mRJqqUuxWJZklHQnvRZV4zdJkDymC0RD9hPhE" />
         
         @if(!empty($noIndex))
