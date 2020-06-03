f533e87a98c31e2880cd1cfc21339b42a9367d98
Gergana <gergana@youpluswe.com>
2020-06-03 18:00:24 +0300

    vox improvements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -513,6 +513,6 @@ class FrontController extends BaseController {
             ]);
         }
 
-        $params['cache_version'] = '2020-06-03-02';
+        $params['cache_version'] = '2020-06-03-03';
     }
 }
