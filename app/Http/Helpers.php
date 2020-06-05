8da4d037cda2b036fc7c0bb094ef800a32f86c8b
Gergana <gergana@youpluswe.com>
2020-06-05 16:35:26 +0300

    vox & trp improvements

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -34,15 +34,3 @@
 		$params['locale'] = $locale ?? App::getLocale();
 		return route($path, $params);
 	}
-
-	function getDentacoinHubApplications() {
-		$fn = storage_path('dentacoin-platforms');
-        $t = file_exists($fn) ? filemtime($fn) : null;
-        if(!$t || $t < time()-300) {
-        	$bla = @file_get_contents('https://dentacoin.com/info/applications');
-        	if( $bla ) {
-            	file_put_contents($fn, $bla);
-        	}
-        }
-        return json_decode(file_get_contents($fn));
-	}
