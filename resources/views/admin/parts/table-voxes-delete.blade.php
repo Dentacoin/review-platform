c4dd787775646c65f27dbbe53f1111a80f4e079b
Gergana <gergana@youpluswe.com>
2020-06-11 12:46:28 +0300

    admin vox bug fixes

---
+++ resources/views/admin/parts/table-voxes-delete.blade.php
@@ -0,0 +1,1 @@
+<a class="btn btn-sm btn-deafult" href="{{ url('cms/'.$current_page.'/delete/'.$item->id) }}" onclick="return confirm('Are you sure you want to DELETE this?');">{{ trans('admin.table.delete') }}</a>
