a6e986f399bae807d4722197a1ed2361fb30a42f
Gergana <gergana@youpluswe.com>
2020-06-12 17:51:45 +0300

    trp email fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -565,7 +565,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
                 $tos
             );
             $email->setTemplateId($item->template->sendgrid_template_id);
-            if($this->is_dentist && $template_id != 58 && $template_id != 59 && $template_id != 60 && $template_id != 61 && $template_id != 62) {
+            if($this->is_dentist && $template_id != 58 && $template_id != 59 && $template_id != 60 && $template_id != 61 && $template_id != 62 && $template_id != 106) {
                 $email->addBcc("4097841@bcc.hubspot.com");
             }
 
