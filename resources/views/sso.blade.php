7c52e95b06238211307e7bb97c9605557274c28f
Gergana <gergana@youpluswe.com>
2020-07-08 12:37:20 +0300

    logout functions fixes

--- resources/views/sso.blade.php
+++ resources/views/sso.blade.php
@@ -1,6 +1,6 @@
 @php($slug = (new \App\Http\Controllers\SSOController())->encrypt($user->id))
 @php($type = (new \App\Http\Controllers\SSOController())->encrypt($user->is_dentist ? 'dentist' : 'patient'))
-@php($token = App\Models\User::getLoginToken())
+@php($token = (new \App\Http\Controllers\SSOController())->getLoginToken())
 <div class="sso" style="display: none;">
 	@foreach( config('platforms') as $k => $platform )
 		@if( !empty($platform['url']) && ( mb_strpos(request()->getHttpHost(), $platform['url'])===false || $platform['url']=='dentacoin.com' )  )
