b6b8d580a3c878dfde880140e03182e482cc053f
Gergana <gergana@youpluswe.com>
2020-08-12 11:24:25 +0300

    cms invites improvements

--- app/Http/Controllers/Admin/InvitesController.php
+++ app/Http/Controllers/Admin/InvitesController.php
@@ -2,18 +2,101 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Validator;
 use App\Http\Controllers\AdminController;
 use App\Models\UserInvite;
-use Illuminate\Http\Request;
+
+use Validator;
+use Request;
 
 
 class InvitesController extends AdminController {
 
     public function list( ) {
 
-    	return $this->showView('invites', array(
-        	'invites' => UserInvite::orderBy('id', 'desc')->take(100)->get(),
+        $items = UserInvite::orderBy('id', 'desc');
+
+        if(!empty(request('search-user-id'))) {
+            $items = $items->where('user_id', request('search-user-id'));
+        }
+
+        if(!empty(request('search-email'))) {
+            $items = $items->whereHas('user', function($query) {
+                $query->where('email', 'LIKE', '%'.trim(request('search-email')).'%');
+            });
+        }
+
+        if(!empty(request('search-name'))) {
+            $items = $items->whereHas('user', function($query) {
+                $query->where('name', 'LIKE', '%'.trim(request('search-name')).'%');
+            });
+        }
+
+        if(!empty(request('search-invited-id'))) {
+            $items = $items->whereHas('invited', function($query) {
+                $query->where('id', request('search-invited-id'));
+            });
+        }
+
+        if(!empty(request('search-invited-email'))) {
+            $items = $items->where('invited_email', 'LIKE', '%'.trim(request('search-invited-email')).'%');
+        }
+
+        if(!empty(request('search-invited-name'))) {
+            $items = $items->where('invited_name', 'LIKE', '%'.trim(request('search-invited-name')).'%');
+        }
+
+        $total_count = $items->count();
+
+        $page = max(1,intval(request('page')));
+        
+        $ppp = 50;
+        $adjacents = 2;
+        $total_pages = ceil($total_count/$ppp);
+
+        //Here we generates the range of the page numbers which will display.
+        if($total_pages <= (1+($adjacents * 2))) {
+          $start = 1;
+          $end   = $total_pages;
+        } else {
+          if(($page - $adjacents) > 1) { 
+            if(($page + $adjacents) < $total_pages) { 
+              $start = ($page - $adjacents);            
+              $end   = ($page + $adjacents);         
+            } else {             
+              $start = ($total_pages - (1+($adjacents*2)));  
+              $end   = $total_pages;               
+            }
+          } else {               
+            $start = 1;                                
+            $end   = (1+($adjacents * 2));             
+          }
+        }
+
+        $items = $items->skip( ($page-1)*$ppp )->take($ppp)->get();
+
+        $pagination_link = '';
+
+        foreach (Request::all() as $key => $value) {
+            if($key != 'search') {
+                $pagination_link .= '&'.$key.'='.($value === null ? '' : $value);
+            }
+        }
+
+        return $this->ShowView('invites', array(
+            'items' => $items,
+            'search_user_id' => request('search-user-id'),
+            'search_email' => request('search-email'),
+            'search_name' => request('search-name'),
+            'search_invited_id' => request('search-invited-id'),
+            'search_invited_email' => request('search-invited-email'),
+            'search_invited_name' => request('search-invited-name'),
+            'total_count' => $total_count,
+            'count' =>($page - 1)*$ppp ,
+            'start' => $start,
+            'end' => $end,
+            'total_pages' => $total_pages,
+            'page' => $page,
+            'pagination_link' => $pagination_link,
         ));
     }
 
--- resources/views/admin/invites.blade.php
+++ resources/views/admin/invites.blade.php
@@ -12,6 +12,46 @@
     <div class="col-md-12">
         <div class="panel panel-inverse">
             <div class="panel-heading">
+                <div class="panel-heading-btn">
+                    <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                </div>
+                <h4 class="panel-title"> Invites filter</h4>
+            </div>
+            <div class="panel-body users-filters">
+                <form method="get" action="{{ url('cms/'.$current_page) }}" id="users-filter-form">
+                    <div class="row" style="margin-bottom: 10px;">
+                        <div class="col-md-1">
+                            <input type="text" class="form-control" name="search-user-id" value="{{ $search_user_id }}" placeholder="Inviter ID">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" name="search-email" value="{{ $search_email }}" placeholder="Inviter Email">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" name="search-name" value="{{ $search_name }}" placeholder="Inviter Name">
+                        </div>
+                        <div class="col-md-1">
+                            <input type="text" class="form-control" name="search-invited-id" value="{{ $search_invited_id }}" placeholder="Invited ID">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" name="search-invited-email" value="{{ $search_invited_email }}" placeholder="Invited Email">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" name="search-invited-name" value="{{ $search_invited_name }}" placeholder="Invited Name">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="submit" class="btn btn-sm btn-primary btn-block" name="search" value="Search">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-inverse">
+            <div class="panel-heading">
                 <h4 class="panel-title">Invites</h4>
             </div>
             <div class="panel-body" id="link" link="{{ url('cms/update-blog-preferences') }}">
@@ -20,20 +60,28 @@
                     <table class="table table-striped table-question-list">
                         <thead>
                             <tr>
-                                <th>From</th>
-                                <th>To</th>
+                                <th>Inviter</th>
+                                <th>Invited email</th>
+                                <th>Invited name</th>
+                                <th>Registered from invite</th>
                                 <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
-                        	@foreach($invites as $invite)
+                        	@foreach($items as $invite)
 	                            <tr>
 	                                <td>
 	                                    <a href="{{ url('cms/users/edit/'.$invite->user_id) }}"> {{ !empty($invite->user) ? $invite->user->name : 'unknown' }}</a>
 	                                </td>
-	                                <td>
-	                                    {!! $invite->invited_email !!}
-	                                </td>
+                                    <td>
+                                        {!! $invite->invited_email !!}
+                                    </td>
+                                    <td>
+                                        {!! $invite->invited_name !!}
+                                    </td>
+                                    <td>
+                                        {!! $invite->invited_id ? '<a href="'.url('cms/users/edit/'.$invite->invited_id).'">'.(!empty($invite->invited) ? $invite->invited->name : 'unknown').'</a>' : '-' !!}
+                                    </td>
                                     <td>
                                         <a class="btn btn-sm btn-deafult" href="{{ url('cms/invites/delete/'.$invite->id) }}" onclick="return confirm('Are you sure you want to DELETE this?');">{{ trans('admin.table.delete') }}</a>
                                     </td>
@@ -46,5 +94,33 @@
         </div>
     </div>
 </div>
+
+@if($total_pages > 1)
+    <nav aria-label="Page navigation" style="text-align: center;">
+        <ul class="pagination">
+            <li class="{{ ($page <= 1 ?  'disabled' : '' ) }}">
+                <a class="page-link" href="{{ url('cms/invites/?page=1'.$pagination_link) }}" aria-label="Previous">
+                    <span aria-hidden="true"> << </span>
+                </a>
+            </li>
+            <li class="{{ ($page <= 1 ?  'disabled' : '' ) }}">
+                <a class="page-link prev" href="{{ url('cms/invites/?page='.($page>1 ? $page-1 : '1').$pagination_link) }}"  aria-label="Previous">
+                    <span aria-hidden="true"> < </span>
+                </a>
+            </li>
+            @for($i=$start; $i<=$end; $i++)
+                <li class="{{ ($i == $page ?  'active' : '') }}">
+                    <a class="page-link" href="{{ url('cms/invites/?page='.$i.$pagination_link) }}">{{ $i }}</a>
+                </li>
+            @endfor
+            <li class="{{ ($page >= $total_pages ? 'disabled' : '') }}">
+                <a class="page-link next" href="{{ url('cms/invites/?page='.($page < $total_pages ? $page+1 :  $total_pages).$pagination_link) }}" aria-label="Next"> <span aria-hidden="true"> > </span> </a>
+            </li>
+            <li class="{{ ($page >= $total_pages ? 'disabled' : '') }}">
+                <a class="page-link" href="{{ url('cms/invites/?page='.$total_pages.$pagination_link) }}" aria-label="Next"> <span aria-hidden="true"> >> </span>  </a>
+            </li>
+        </ul>
+    </nav>
+@endif
 
 @endsection
