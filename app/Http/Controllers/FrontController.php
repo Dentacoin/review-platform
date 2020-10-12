79947e8e032e98c55b6bb4903bc41bcfb1956e9c
Gergana <gergana@youpluswe.com>
2020-10-12 13:00:16 +0300

    trp optimization

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -594,6 +594,6 @@ class FrontController extends BaseController {
             ]);
         }
 
-        $params['cache_version'] = '20201012';
+        $params['cache_version'] = '2020101201';
     }
 }
