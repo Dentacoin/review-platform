7262c9d43735a4815617e05be0ea5ba26cd83b77
Gergana <gergana@youpluswe.com>
2021-01-05 18:15:16 +0200

    test

--- app/Http/Controllers/Front/YouTubeController.php
+++ app/Http/Controllers/Front/YouTubeController.php
@@ -54,7 +54,7 @@ class YouTubeController extends FrontController {
         set_time_limit(0);
         ini_set('memory_limit','1024M');
 
-            $users = User::where('id', '>', 106035)->withTrashed()->get();
+            $users = User::withTrashed()->get();
 
             $ids = [];
             foreach($users as $user) {
