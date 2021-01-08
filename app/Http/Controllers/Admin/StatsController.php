3615b2475168a9c2180d53be95a53bca74bb892d
Gergana <gergana@youpluswe.com>
2021-01-08 18:02:46 +0200

    users stats bug fixes

--- app/Http/Controllers/Admin/StatsController.php
+++ app/Http/Controllers/Admin/StatsController.php
@@ -12,7 +12,7 @@ class StatsController extends AdminController
     public function registrations( ) {
         $weeks = DB::select("
             SELECT 
-                CONCAT(YEAR(`created_at`), 'W', LPAD( WEEK( DATE_SUB(  `created_at` , INTERVAL 16 HOUR ) , 5 ) , 2 , '0' ) ) AS  `week`,
+                CONCAT(YEAR(`created_at`), 'W', REPLACE(LPAD( WEEK( DATE_SUB(  `created_at` , INTERVAL 16 HOUR ) , 5 ) , 2 , '0' ) , '00', '52') ) AS  `week`,
                 SUM( IF(  `is_dentist` AND `platform` = 'trp', 1, 0 ) ) AS `dentist`, 
                 SUM( IF(  !`is_dentist` AND `platform` = 'trp' , 1, 0 ) ) AS `patient`, 
                 SUM( IF(  `platform` = 'vox' , 1, 0 ) ) AS `vox`
@@ -22,7 +22,7 @@ class StatsController extends AdminController
         ");
 
         $table = [];
-        //dd($weeks);
+        // dd($weeks);
         foreach ($weeks as $w) {
             $s = date('d.m.Y', strtotime($w->week) - 86400*3 );
             $e = date('d.m.Y', strtotime($w->week) + 86400*4 );
