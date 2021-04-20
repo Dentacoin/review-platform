8493be7029b8a7a5eabfd92b326b5a8ff3021ff9
Gergana <gergana@youpluswe.com>
2021-04-20 10:45:04 +0300

    trp dentist page improvements

--- app/Http/Controllers/Front/DentistController.php
+++ app/Http/Controllers/Front/DentistController.php
@@ -653,7 +653,7 @@ class DentistController extends FrontController {
             $view_params['js'][] = '../js/jquery-ui.min.js';
         }
 
-        if($item->photos->isNotEmpty() && empty($item->reviews_in())) {
+        if($item->photos->isNotEmpty() && $item->reviews_in()->isEmpty()) {
             $load_lightbox = 'true';
             $view_params['css'][] = 'lightbox.css';
             $view_params['js'][] = '../js/lightbox.js';
@@ -662,7 +662,7 @@ class DentistController extends FrontController {
         }
         $view_params['load_lightbox'] = $load_lightbox;
 
-        if((!empty($this->user) && $this->user->id == $item->id) || ($item->photos->isNotEmpty() || $item->teamApproved->isNotEmpty() || $item->invites_team_unverified->isNotEmpty()) && empty($item->reviews_in())) {
+        if((!empty($this->user) && $this->user->id == $item->id) || ($item->photos->isNotEmpty() || $item->teamApproved->isNotEmpty() || $item->invites_team_unverified->isNotEmpty()) && $item->reviews_in()->isEmpty()) {
             $load_flickity = 'true';
             $view_params['css'][] = 'flickity.min.css';
             $view_params['js'][] = 'flickity.min.js';
