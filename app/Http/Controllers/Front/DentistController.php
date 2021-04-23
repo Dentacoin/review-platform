5edbd50db61cfa53599a7eb6729b5aaf6ce4e6f5
Gergana <gergana@youpluswe.com>
2021-04-23 17:36:52 +0300

    trp clinic branches

--- app/Http/Controllers/Front/DentistController.php
+++ app/Http/Controllers/Front/DentistController.php
@@ -2157,7 +2157,9 @@ Link to patients\'s profile in CMS: https://reviews.dentacoin.com/cms/users/edit
         $imgs_urls = [];
         foreach( config('platforms') as $k => $platform ) {
             if( !empty($platform['url']) && ( mb_strpos(request()->getHttpHost(), $platform['url'])===false || $platform['url']=='dentacoin.com' )  ) {
-                $imgs_urls[] = '//'.$platform['url'].'/custom-cookie?logout-token='.urlencode($token);
+                if($k !== 'vox' && $k !== 'trp') {
+                    $imgs_urls[] = '//'.$platform['url'].'/custom-cookie?logout-token='.urlencode($token);
+                }
             }
         }
         $imgs_urls[] = '//vox.dentacoin.com/custom-cookie?logout-token='.urlencode($token);
@@ -2190,7 +2192,9 @@ Link to patients\'s profile in CMS: https://reviews.dentacoin.com/cms/users/edit
                     $imgs_urls = [];
                     foreach( config('platforms') as $k => $platform ) {
                         if( !empty($platform['url']) && ( mb_strpos(request()->getHttpHost(), $platform['url'])===false || $platform['url']=='dentacoin.com' )  ) {
-                            $imgs_urls[] = '//'.$platform['url'].'/custom-cookie?slug='.urlencode(User::encrypt($item->id)).'&type='.urlencode(User::encrypt('dentist')).'&token='.urlencode($token);
+                            if($k !== 'vox' && $k !== 'trp') {
+                                $imgs_urls[] = '//'.$platform['url'].'/custom-cookie?slug='.urlencode(User::encrypt($item->id)).'&type='.urlencode(User::encrypt('dentist')).'&token='.urlencode($token);
+                            }
                         }
                     }
 
