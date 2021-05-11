f277cd4979917649225954f0bbb1d6b3aa4a5778
Gergana <gergana@youpluswe.com>
2021-05-11 15:19:57 +0300

    trp clinic branches improvements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -595,6 +595,6 @@ class FrontController extends BaseController {
             ]);
         }
 
-        $params['cache_version'] = '2021051102';
+        $params['cache_version'] = '2021051103';
     }
 }
