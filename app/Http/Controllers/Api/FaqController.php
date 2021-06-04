dd3311adec2fe5586448c1e87972c54add148e10
Gergana <gergana@youpluswe.com>
2021-06-04 17:08:28 +0300

    mobile app faq for ios

--- app/Http/Controllers/Api/FaqController.php
+++ app/Http/Controllers/Api/FaqController.php
@@ -29,9 +29,12 @@ class FaqController extends ApiController {
 	public function getFaq() {
 		$pathToFile = base_path().'/resources/lang/en/faq.php';
         $content = json_decode( file_get_contents($pathToFile), true );
+		$pathToFileiOS = base_path().'/resources/lang/en/faq.php';
+        $contentiOS = json_decode( file_get_contents($pathToFileiOS), true );
 
         return Response::json( array(
 			'faq' => $content,
+			'faq_ios' => $contentiOS,
 		) );
 	}
     
