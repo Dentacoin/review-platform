41ad8c80ce236285454d1d05ab4569e9b9abcfbb
Gergana <gergana@youpluswe.com>
2021-06-11 18:07:23 +0300

    remove unused field

--- app/Models/User.php
+++ app/Models/User.php
@@ -120,7 +120,6 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         'invited_himself_reg',
         'hasimage',
         'hasimage_social',
-        'vox_address',
         'tw_id',
         'gp_id',
         'fb_id',
