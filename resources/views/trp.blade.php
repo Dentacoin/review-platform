5d780b676a888d98baa0916cead45820a72bb181
Gergana <gergana@youpluswe.com>
2021-06-17 17:37:27 +0300

    trp improve page speed

--- resources/views/trp.blade.php
+++ resources/views/trp.blade.php
@@ -38,8 +38,8 @@
 
 		<link rel="stylesheet" type="text/css" href="{{ url('/css/new-style-trp.css').'?ver='.$cache_version }}" />
 		
-		<link rel="preload" href="{{ url('font-awesome/webfonts/fa-brands-400.woff2') }}" as="font" crossorigin>
-		<link rel="preload" href="{{ url('font-awesome/webfonts/fa-solid-900.woff2') }}" as="font" crossorigin>
+		<!-- <link rel="preload" href="{{ url('font-awesome/webfonts/fa-brands-400.woff2') }}" as="font" crossorigin>
+		<link rel="preload" href="{{ url('font-awesome/webfonts/fa-solid-900.woff2') }}" as="font" crossorigin> -->
 		<link rel="preload" href="{{ url('fonts/Calibri-Light.woff2') }}" as="font" crossorigin>
 		<link rel="preload" href="{{ url('fonts/Calibri-Bold.woff2') }}" as="font" crossorigin>
 		<link rel="preload" href="{{ url('fonts/Calibri.woff2') }}" as="font" crossorigin>
