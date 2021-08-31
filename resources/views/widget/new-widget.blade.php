67de64d59714c68f771904991f60863f7f38f908
Gergana <gergana@youpluswe.com>
2021-08-31 17:00:40 +0300

    bug fixes

--- resources/views/widget/new-widget.blade.php
+++ resources/views/widget/new-widget.blade.php
@@ -30,27 +30,29 @@
 	@if($layout == 'carousel')
 		<div class="widget-flickity" id="widget-flickity">
 			@foreach($reviews as $review)
-				<div class="widget-slide">
-					<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-avatar" style="background-image: url('{{ $review->user->getImageUrl(true) }}');"></a>
-					@if($review->title)
-		    			<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-title">
-		    				“{{ $review->title }}”
-		    			</a>
-	    			@endif
-	    			<div class="ratings average">
-						<div class="stars">
-							<div class="bar" style="width: {{ $review->rating/5*100 }}%;">
+				@if($review->user)
+					<div class="widget-slide">
+						<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-avatar" style="background-image: url('{{ $review->user->getImageUrl(true) }}');"></a>
+						@if($review->title)
+							<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-title">
+								“{{ $review->title }}”
+							</a>
+						@endif
+						<div class="ratings average">
+							<div class="stars">
+								<div class="bar" style="width: {{ $review->rating/5*100 }}%;">
+								</div>
 							</div>
 						</div>
+						<div class="review-content">
+							{!! nl2br($review->answer) !!}
+						</div>
+						<p class="review-name">{{ !empty($review->user->self_deleted) ? ($review->verified ? 'Verified Patient' : 'Deleted User') : $review->user->name }}</p>
+						<p class="review-date">
+							{{ $review->created_at ? date('d/m/Y', $review->created_at->timestamp) : '-' }}
+						</p>
 					</div>
-					<div class="review-content">
-						{!! nl2br($review->answer) !!}
-					</div>
-					<p class="review-name">{{ !empty($review->user->self_deleted) ? ($review->verified ? 'Verified Patient' : 'Deleted User') : $review->user->name }}</p>
-					<p class="review-date">
-						{{ $review->created_at ? date('d/m/Y', $review->created_at->timestamp) : '-' }}
-					</p>
-				</div>
+				@endif
 			@endforeach
 		</div>
 
@@ -146,34 +148,36 @@
 		<div class="list-reviews">
 
 			@foreach($reviews as $review)
-				<div class="list-review">
-					<div class="list-review-left">
-						<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-avatar" style="background-image: url('{{ $review->user->getImageUrl(true) }}');"></a>
-						<span class="review-date">
-							{{ $review->created_at ? date('d/m/Y', $review->created_at->timestamp) : '-' }}
-						</span>
-					</div>
-					<div class="list-review-right">
-						@if($review->title)
-			    			<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-title">
-			    				“{{ $review->title }}”
-			    			</a>
-		    			@endif
-		    			<div class="ratings">
-							<div class="stars">
-								<div class="bar" style="width: {{ $review->rating/5*100 }}%;">
+				@if($review->user)
+					<div class="list-review">
+						<div class="list-review-left">
+							<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-avatar" style="background-image: url('{{ $review->user->getImageUrl(true) }}');"></a>
+							<span class="review-date">
+								{{ $review->created_at ? date('d/m/Y', $review->created_at->timestamp) : '-' }}
+							</span>
+						</div>
+						<div class="list-review-right">
+							@if($review->title)
+								<a href="{{ $user->getLink().'?review_id='.$review->id }}" target="_blank" class="review-title">
+									“{{ $review->title }}”
+								</a>
+							@endif
+							<div class="ratings">
+								<div class="stars">
+									<div class="bar" style="width: {{ $review->rating/5*100 }}%;">
+									</div>
 								</div>
 							</div>
+							<div class="review-content">
+								{!! nl2br($review->answer) !!}
+							</div>
+							<span class="review-name">{{ !empty($review->user->self_deleted) ? ($review->verified ? 'Verified Patient' : 'Deleted User') : $review->user->name }}</span>
+							<span class="mobile-review-date">
+								{{ $review->created_at ? date('d/m/Y', $review->created_at->timestamp) : '-' }}
+							</span>
 						</div>
-						<div class="review-content">
-							{!! nl2br($review->answer) !!}
-						</div>
-						<span class="review-name">{{ !empty($review->user->self_deleted) ? ($review->verified ? 'Verified Patient' : 'Deleted User') : $review->user->name }}</span>
-						<span class="mobile-review-date">
-							{{ $review->created_at ? date('d/m/Y', $review->created_at->timestamp) : '-' }}
-						</span>
 					</div>
-				</div>
+				@endif
 	    	@endforeach
 		</div>
 
