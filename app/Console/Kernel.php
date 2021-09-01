b060bfbbd604978cb73d980d982f198c98c7e831
Gergana <gergana@youpluswe.com>
2021-09-01 11:15:34 +0300

    answered questions count

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2448,7 +2448,7 @@ PAID BY USER NOTIFICATION FOR TRANSACTIONS
 
             echo 'Answered questions count cron - DONE!'.PHP_EOL.PHP_EOL.PHP_EOL;
             
-        })->dailyAt('09:16');
+        })->dailyAt('08:17');
 
 
         $schedule->call(function () {
