28970c06b5cb6456882d8952b3939d14246729e3
Gergana <gergana@youpluswe.com>
2021-09-02 11:00:34 +0300

    bug fixes

--- app/Models/VoxAnswer.php
+++ app/Models/VoxAnswer.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Carbon\Carbon;
 
 class VoxAnswer extends Model {
+
+    use SoftDeletes;
     
     protected $fillable = [
         'user_id',
