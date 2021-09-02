810c47cd3801d12fe166fe5c5c16321f8de5b3f8
Gergana <gergana@youpluswe.com>
2021-09-02 10:56:07 +0300

    bug fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2448,7 +2448,7 @@ PAID BY USER NOTIFICATION FOR TRANSACTIONS
 
             echo 'Answered questions count cron 11 - DONE!'.PHP_EOL.PHP_EOL.PHP_EOL;
             
-        })->dailyAt('07:49');
+        })->dailyAt('07:57');
 
 
         $schedule->call(function () {
--- app/Models/VoxAnswer.php
+++ app/Models/VoxAnswer.php
@@ -33,6 +33,7 @@ class VoxAnswer extends Model {
     protected $dates = [
         'created_at',
         'updated_at',
+        'deleted_at',
     ];
 
     public function user() {
