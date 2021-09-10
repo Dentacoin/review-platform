d9354c117a0c96d2b7b0ba3a7760fcd1216eaad4
Gergana <gergana@youpluswe.com>
2021-09-10 12:28:34 +0300

    trp page speed improvements

--- resources/views/trp/user.blade.php
+++ resources/views/trp/user.blade.php
@@ -439,10 +439,6 @@
 		</div>
     </div>
 
-	@if($item->id == 37530)
-		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuriesLorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
-	@endif
-
     <div class="profile-tabs {!! $item->reviews_in_standard()->count() && $item->reviews_in_video()->count() && (!empty($user) && $user->id==$item->id && ($user->patients_invites->isNotEmpty() || $user->asks->isNotEmpty())) ? 'full-tabs' : '' !!}">
     	@if( $item->reviews_in_standard()->count() )
 	    	<a class="tab" data-tab="reviews" href="javascript:;" style="z-index: 5;">
