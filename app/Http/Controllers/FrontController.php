02b67ec525662ee1f9f0b33840ebb22cfada0d99
Gergana <gergana@youpluswe.com>
2021-09-10 11:52:13 +0300

    trp page speed improvements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -632,6 +632,6 @@ class FrontController extends BaseController {
             ]);
         }
 
-        $params['cache_version'] = '2021090909';
+        $params['cache_version'] = '20210910';
     }
 }
