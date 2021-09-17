5d827f488221910d51166c001a58e68b4e9af5a2
Gergana <gergana@youpluswe.com>
2021-09-17 12:16:55 +0300

    vox page speed improvements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -632,6 +632,6 @@ class FrontController extends BaseController {
             ]);
         }
 
-        $params['cache_version'] = '20210917';
+        $params['cache_version'] = '2021091701';
     }
 }
