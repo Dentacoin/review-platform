f231c208761874e06de501226d65e6b5245e2c21
Gergana <gergana@youpluswe.com>
2021-10-27 14:33:14 +0300

    trp design improvements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -634,6 +634,6 @@ class FrontController extends BaseController {
             ]);
         }
 
-        $params['cache_version'] = '2021102701';
+        $params['cache_version'] = '2021102702';
     }
 }
--- resources/views/trp/popups/invite-new-dentist-success.blade.php
+++ resources/views/trp/popups/invite-new-dentist-success.blade.php
@@ -1,12 +1,19 @@
-<div class="popup fixed-popup invite-new-dentist-success-popup active removable" id="invite-new-dentist-success-popup">
+<div class="popup fixed-popup invite-new-dentist-success-popup active removable" id="invite-new-dentist-success-popup" scss-load="trp-popup-invite-new-dentist">
 	<div class="popup-inner inner-white">
 		<a href="javascript:;" class="close-popup">
 			<img src="{{ url('img/close-icon.png') }}"/>
 		</a>
 		<img src="{{ url('img-trp/verification-check.png') }}">
 		<h2>
-			{!! trans('trp.page.invite.popup.success.title') !!}
+			@if(!empty($user))
+				{!! trans('trp.page.invite.popup.success.title-new', ['name' => $user->getNames()]) !!}
+			@else
+				{!! trans('trp.page.invite.popup.success.title-new-no-user') !!}
+			@endif
 		</h2>
+		<p>
+			{!! trans('trp.page.invite.popup.success.title') !!}
+		</p>
 		<p>
 			@if(!empty($user))
 				{!! nl2br(trans('trp.page.invite.popup.success.text', [ 'name' => '<span id="inv_dent_name">[Clinic/Dentist Name]</span>' ,'amount' => '<span>'.App\Models\Reward::getReward('patient_add_dentist').' DCN</span>'])) !!}
@@ -15,7 +22,7 @@
 			@endif
 		</p>
 		@if(!empty($user))
-			<a href="javascript:;" class="button close-popup">{!! trans('trp.page.invite.popup.success.button') !!}</a>
+			<a href="javascript:;" class="button close-popup">{!! trans('trp.page.invite.popup.success.button-back') !!}</a>
 		@else
 			<a href="javascript:;" class="button close-popup open-dentacoin-gateway patient-login">{!! trans('trp.page.invite.popup.success.sign-up') !!}</a>
 		@endif
