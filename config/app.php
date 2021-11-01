f79952dcd26020417f719beb89631e34e40c408a
Gergana <gergana@youpluswe.com>
2021-11-01 14:05:06 +0200

    admin improvements

--- config/app.php
+++ config/app.php
@@ -110,7 +110,8 @@ return [
 
     'key' => env('APP_KEY'),
 
-    'cipher' => 'aes-256-cbc',
+    'cipher' => env('CRYPTO_METHOD'),
+    // 'cipher' => 'aes-256-cbc',
 
     /*
     |--------------------------------------------------------------------------
