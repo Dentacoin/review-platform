776130dbb9723793107c4b0b95cc86d8ceb44936
Gergana <gergana@youpluswe.com>
2022-01-07 13:16:12 +0200

    cms transactions improvement

--- resources/views/admin/parts/table-transactions-bump.blade.php
+++ resources/views/admin/parts/table-transactions-bump.blade.php
@@ -1,10 +1,10 @@
 @if($admin->role!='support')
-	@if($item->status == 'stopped' || $item->status == 'dont_retry' || $item->status == 'pending' )
+	@if(in_array($item->status, ['stopped','dont_retry','pending']) )
 		<a class="btn btn-primary" href="{{ url('cms/transactions/bump/'.$item->id) }}" style="margin-top: 2px;">
 			Bump
 		</a>
 	@endif
-	@if($item->status != 'completed' && $item->status != 'unconfirmed' && $item->status != 'stopped' && $item->status != 'dont_retry' && $item->status != 'failed')
+	@if(!in_array($item->status, ['completed','unconfirmed','stopped','dont_retry','failed','first']))
 		<a class="btn btn-danger" href="{{ url('cms/transactions/stop/'.$item->id) }}" style="margin-top: 2px;">
 			Stop
 		</a>
@@ -16,7 +16,7 @@
 	@endif
 @endif
 @if($admin->role='super_admin')
-	@if($item->status != 'completed' && $item->status != 'unconfirmed' && $item->status != 'pending' && $item->status != 'failed')
+	@if(!in_array($item->status, ['completed','unconfirmed','pending','failed']))
 		<a class="btn btn-info" onclick="return confirm('Are you sure you want to DELETE this?');" href="{{ url('cms/transactions/delete/'.$item->id) }}" style="background: black;border-color: black;margin-top: 2px;">
 			Delete
 		</a>
