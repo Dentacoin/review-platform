a264b6a18f5e6b7cdcfcaf038a8152872ddff58e
Gergana <gergana@youpluswe.com>
2022-01-18 10:22:41 +0200

    admin improvements

--- resources/views/admin/parts/table-users-name.blade.php
+++ resources/views/admin/parts/table-users-name.blade.php
@@ -7,7 +7,7 @@
 		</a>
 	@else
 		<a href="{{ url('/cms/users/users/edit/'.$item->id) }}">
-			{{ !empty($item) ? $item->getNames() : 'Deleted user' }}
+			{{ !empty($item) ? $item->name : 'Deleted user' }}
 		</a>
 
 	@endif
