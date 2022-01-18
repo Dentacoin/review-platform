3b663a41e0a815665bf1f319824ef169ea2fe11c
Gergana <gergana@youpluswe.com>
2022-01-18 10:18:38 +0200

    admin improvements

--- resources/views/admin/parts/table-users-name.blade.php
+++ resources/views/admin/parts/table-users-name.blade.php
@@ -1,7 +1,17 @@
 @if($item->id == 113928)
 	Email sender for unregistered users	
 @else
-	<a href="{{ url('/cms/users/users/edit/'.$item->id) }}">
-		{{ !empty($item) ? $item->getNames() : 'Deleted user' }}
-	</a>
+	@if(!empty($item))
+		@if(!empty($item->user))
+			<a href="{{ url('/cms/users/users/edit/'.$item->user_id) }}">
+				{{ !empty($item->user) ? $item->user->getNames() : 'Deleted user' }}
+			</a>
+		@else
+			<a href="{{ url('/cms/users/users/edit/'.$item->id) }}">
+				{{ !empty($item) ? $item->getNames() : 'Deleted user' }}
+			</a>
+
+		@endif
+	@endif
+	
 @endif
