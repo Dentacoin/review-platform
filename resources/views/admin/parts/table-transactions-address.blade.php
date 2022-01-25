d8d80c0452d78db674a28d9b7adde94f0eca7730
Gergana <gergana@youpluswe.com>
2022-01-25 12:13:49 +0200

    admin improvements

--- resources/views/admin/parts/table-transactions-address.blade.php
+++ resources/views/admin/parts/table-transactions-address.blade.php
@@ -1,1 +1,3 @@
-{{ $item->address }} {!! $item->userWalletAddress ? ($item->userWalletAddress->is_deprecated ? '' : '<img src="'.url('img/alert-small-success.png').'"') : '' !!}
+<div style="display: flex;align-items: center;"> 
+    {{ $item->address }} {!! $item->userWalletAddress ? ($item->userWalletAddress->is_deprecated ? '' : '<img style="max-width: 13px;margin-left: 10px;" src="'.url('img/alert-small-success.png').'"') : '' !!}
+</div>
