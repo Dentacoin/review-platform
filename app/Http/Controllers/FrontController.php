86457e664a0c616ec0afb8ebbd2732e5e8eeed15
Gergana <gergana@youpluswe.com>
2022-06-07 17:04:45 +0300

    trp redesign improvements

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -624,6 +624,6 @@ class FrontController extends BaseController {
             ]);
         }
 
-        $params['cache_version'] = '2022060704';
+        $params['cache_version'] = '2022060705';
     }
 }
