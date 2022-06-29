1516ba19caacaab3abf7fdebe548c1c3e4621929
Gergana <gergana@youpluswe.com>
2022-06-29 11:56:12 +0300

    trp page speed improvements

--- resources/views/trp/user.blade.php
+++ resources/views/trp/user.blade.php
@@ -930,41 +930,43 @@
 
 		{{-- <div id="to-append"></div> --}}
 		
-		@if($showTeamSection)
-			@include('trp.user.team')
-		@endif
-		
-		@if($regularReviewsCount || $videoReviewsCount )
-			@include('trp.user.reviews')
-		@endif
-		
-		@if( $showLocationsSection )
-			@include('trp.user.location')
-		@endif
-		
-		@if($showMoreInfoSection)
-			@include('trp.user.more')
-		@endif
+		@if($item->id != 37530)
+			@if($showTeamSection)
+				@include('trp.user.team')
+			@endif
+			
+			@if($regularReviewsCount || $videoReviewsCount )
+				@include('trp.user.reviews')
+			@endif
+			
+			@if( $showLocationsSection )
+				@include('trp.user.location')
+			@endif
+			
+			@if($showMoreInfoSection)
+				@include('trp.user.more')
+			@endif
 
-		@if($item->highlights->isNotEmpty())
-			<div class="tab-container">
-				<h2 class="mont">
-					Highlights
-				</h2>
+			@if($item->highlights->isNotEmpty())
+				<div class="tab-container">
+					<h2 class="mont">
+						Highlights
+					</h2>
 
-				<div class="tab-inner-section">
-					<div class="hightlights-wrapper {{ $item->highlights->count() > 1 ? 'highlights-mobile-flickity' : '' }} {{ $item->highlights->count() > 3 ? 'highlights-flickity' : 'flex' }}">
-						@foreach($item->highlights as $highlight)
-							<a href="{{ $highlight->link }}" target="_blank" class="hightlight">
-								<div class="hightlight-image">
-									<img src="{{ $highlight->getImageUrl() }}"/>
-								</div>
-								<p>{{ $highlight->title }}</p>
-							</a>
-						@endforeach
+					<div class="tab-inner-section">
+						<div class="hightlights-wrapper {{ $item->highlights->count() > 1 ? 'highlights-mobile-flickity' : '' }} {{ $item->highlights->count() > 3 ? 'highlights-flickity' : 'flex' }}">
+							@foreach($item->highlights as $highlight)
+								<a href="{{ $highlight->link }}" target="_blank" class="hightlight">
+									<div class="hightlight-image">
+										<img src="{{ $highlight->getImageUrl() }}"/>
+									</div>
+									<p>{{ $highlight->title }}</p>
+								</a>
+							@endforeach
+						</div>
 					</div>
 				</div>
-			</div>
+			@endif
 		@endif
 
 		<input type="hidden" name="cur_dent_id" id="cur_dent_id" value="{{ $item->id }}">
